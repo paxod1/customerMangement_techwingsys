6f2cd5f99d29983c198bc2d74005fa41148fdbd9
chiku binoy <chikubinoybinoy@gmail.com>
2025-02-25 10:31:05 +0530

    sg

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -6,13 +6,16 @@ import { persistor, store } from './Redux/store.js'
 import { Provider } from 'react-redux'
 import { PersistGate } from 'redux-persist/integration/react'
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Provider store={store}>
-      <PersistGate persistor={persistor}>
-        <App />
-      </PersistGate>
-    </Provider>
+    <BrowserRouter>
+      <Provider store={store}>
+        <PersistGate persistor={persistor}>
+          <App />
+        </PersistGate>
+      </Provider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
